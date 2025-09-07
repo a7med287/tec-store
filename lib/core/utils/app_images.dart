f49b3883f9bc0538553ad100614eef833e12717e
Ahmed Rizk <rezk18649@gmail.com>
2025-09-07 22:32:18 +0300

    create splash screen ui

---
+++ lib/core/utils/app_images.dart
@@ -0,0 +1,6 @@
+class Assets{
+  static const String logo = "assets/images/logo.png";
+  static const String whiteLogo = "assets/images/white_logo.png";
+
+
+}
---
+++ lib/features/splash/presetation/views/splash_view.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:tec_store/core/utils/app_colors.dart';
+import 'package:tec_store/features/splash/presetation/views/splash_view_body.dart';
+
+class SplashView extends StatelessWidget {
+  const SplashView({super.key});
+
+  static const String routName = "splashView";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors.primaryColor,
+      body: const SplashViewBody(),
+    );
+  }
+}
+
